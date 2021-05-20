7f55234f941eef974f54d6f9e0f00b50e224b4a2
Jenni Davila <pazjenni1331@gmail.com>
2021-05-19 19:30:26 -0500

    updated functions and created global variables

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "[", "]", "{", "}", "+", "=", "-", "_", ".", ","];
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var randomValue = symbol[Math.floor(Math.random() * symbol.length)];
 
-//generates a random upper letter
-function randomUpperCase() {
-  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let randomValue = upperLetters[Math.floor(Math.random() * upperLetters.length)];
-  
-  return console.log(randomValue);
-
-}
-
-//generates a random lower letter
-function randomLowerCase() {
-  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let randomValue = lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-
-  return console.log(randomValue);
-}
-
-//generates a random number
-function randomNumber() {
-  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  let randomValue = number[Math.floor(Math.random() * number.length)];
-
-  return console.log(randomValue);
-}
-
-//generates a random symbol
-function randomSymbol() {
-  var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", "[", "]", "{", "}", "+", "=", "-", "_", ".", ","];
-  let randomValue = symbol[Math.floor(Math.random() * symbol.length)];
-
-  return console.log(randomValue);
-}
-  
-
-randomLowerCase();
-randomUpperCase();
-randomNumber();
-randomSymbol();
+//prompt to user how long they choose length?
 
 
-//generatePassword(upperCase.length);
-
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-//}
 
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
-//writePassword();
+
+
+
+
+function generatePassword() {
+  var password = " "; //this variable expects some value to it
+  var passLength= prompt("How many characters would you like your password?  8 characters min - 128 characters max.");
+  var isUpperCase = confirm ("Do you want it to contain upper case letters?");
+  for(var i=0; i < password.length; i++){
+
+  }
+
+  if (passLength < 8 || passLength > 128) {
+    prompt("Password must be 8 - 128 characters.");
+  }
+
+  return passwordLength;
+}
+
+
+
+
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+}
+
+
+
+
+writePassword();
